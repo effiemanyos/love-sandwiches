1fa07b88ad1912e36fc417578b8d16d8ad81ab6d
Effie Manyos <fmanyos@gmail.com>
2021-07-10 12:18:24 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -19,7 +19,10 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
 
     # while loop - these lines of code will keep running until the data provided is valid
@@ -30,7 +33,8 @@ def get_sales_data():
         print("Example: 10, 20, 30, 40, 50, 60\n")
 
         data_str = input("Enter your data here: ") # Asks the user the data
-        # print(f"The data provided is {data_str}") # String value provided by users, we can remove this after testing
+        # print(f"The data provided is {data_str}")
+        # String value provided by users, we can remove this after testing
 
         sales_data = data_str.split(",") # Converts the string of data from the user into a list of values
         validate_data(sales_data)
@@ -64,4 +68,16 @@ def validate_data(values):
     return True
 
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n") # The \n adds a space between paragraphs
+    sales_worksheet = SHEET.worksheet("sales") # We are using the worksheet method to acces our "sales" worksheet in the spreadsheet
+    sales_worksheet.append_row(data) # The append_row method adds a new row to the end of our data in the worksheet selected
+    print("Sales worksheet updated successfully!\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data) # We need to call our function and pass it our sales_data list
