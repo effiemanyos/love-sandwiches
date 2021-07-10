fcdf0b659c53b43e071800f2b3fe23a6e211dd01
Effie Manyos <fmanyos@gmail.com>
2021-07-10 14:38:24 +0000

    Refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -91,6 +91,17 @@ def update_surplus_worksheet(data):
     print("Surplus worksheet updated successfully!\n")
 
 
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully!\n")
+
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -122,13 +133,13 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data) # We need to call our function and pass it our sales_data list
-    new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(sales_data, "sales") # We need to call our function and pass it our sales_data list
+    new_surplus_data = calculate_surplus_data(sales_data) # Calculates our surplus data and returns it to the new surplus data variable
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation\n")
 main()
 
 # stock - sales = surplus
-# This are methods from the gspread library
+# These are methods from the gspread library
