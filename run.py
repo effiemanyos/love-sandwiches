f95a7a91ab9b73e2fb80d3f3a8076a2096c457d5
Effie Manyos <fmanyos@gmail.com>
2021-07-10 15:55:14 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -107,7 +107,7 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-def gest_last_5_entries_sales():
+def get_last_5_entries_sales():
     """
     Collects columns of data from sales worksheet, collecting the last
     5 entries for each sandwich and returns the data as a list of lists.
@@ -124,6 +124,21 @@ def gest_last_5_entries_sales():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10% 
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data: 
+        int_column = [int(num) for num in column] # We are converting our values into integers
+        average = sum(int_column) / len(int_column) # This expression would also work sum(int_column)/5 as we know our stock will always have 5 items
+        stock_num = average * 1.1 # This will add 10% to the average
+        new_stock_data.append(round(stock_num))
+        
+    return new_stock_data
+
 def main():
     """
     Run all program functionsm
@@ -133,12 +148,13 @@ def main():
     update_worksheet(sales_data, "sales") # We need to call our function and pass it our sales_data list
     new_surplus_data = calculate_surplus_data(sales_data) # Calculates our surplus data and returns it to the new surplus data variable
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print("Welcome to Love Sandwiches Data Automation\n")
-# main() # Handy way to test an individual function - by commenting out the main() function for it not to run at the same time
-
-sales_columns = get_last_5_entries_sales()
+main() # Handy way to test an individual function - by commenting out the main() function for it not to run at the same time
 
 # stock - sales = surplus
 # These are methods from the gspread library
